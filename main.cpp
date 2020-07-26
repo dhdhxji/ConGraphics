af41e0e76d5b916f8fd21380c0cf8f2d0693cdc3
Oleksandr <keysindie@gmail.com>
2020-07-26 21:54:34 +0300

    Change demo for use cube class instead of pure vectors

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include "console_canvas.h"
-#include "glm/vec3.hpp"
 #include <vector>
 #include <glm/gtc/matrix_transform.hpp>
 #include <chrono>
 #include <thread>
+
+#include "console_canvas.h"
+#include "cube.h"
+#include "glm/vec3.hpp"
 
 #define MAX_FPS 30
 
@@ -28,23 +30,12 @@ int main()
 {
     Canvas c;
 
-    const glm::vec3 cube_vertices[8] = {
-        //front side
-        glm::vec3(-.5f, .5f, .5f),
-        glm::vec3(.5f, .5f, .5f),
-        glm::vec3(.5f, -.5f, .5f),
-        glm::vec3(-.5f, -.5f, .5f),
-        //back side
-        glm::vec3(-.5f, .5f, -.5f),
-        glm::vec3(.5f, .5f, -.5f),
-        glm::vec3(.5f, -.5f, -.5f),
-        glm::vec3(-.5f, -.5f, -.5f),
-    };
+    Cube cube(&c, glm::vec3(0.f, 0.f, 0.f), 1.f);
 
     //degrees per second
     const float yaw_speed = 10; 
-    const float pitch_speed = 1;
-    const float roll_speed = 0;
+    const float pitch_speed = 5;
+    const float roll_speed = 2;
 
     float yaw = 0;
     float pitch = 0;
@@ -62,74 +53,32 @@ int main()
         time = std::chrono::high_resolution_clock::now();
 
 
-        const uint64_t time_for_sleep_ms = (frame_time_ms > prev_frame_time) ?
-                                           frame_time_ms - prev_frame_time : 0;
-
-        std::this_thread::sleep_for(std::chrono::duration<uint64_t, std::milli>(time_for_sleep_ms));
-
-
         c.clear_screen();
-       
+
         c.draw_string(glm::vec3(-1.f, 1.f, 1.f), "FPS: " + std::to_string(1000.f/prev_frame_time));
         c.draw_string(glm::vec3(-1.f, .9f, 1.f), "Frame time: " + std::to_string(prev_frame_time));
 
-        glm::mat4 transform_matrix(1.f);
-        transform_matrix = glm::rotate(transform_matrix, 
-                                       glm::radians(yaw), 
-                                       glm::vec3(0.f, 0.f, 1.f));
 
-        transform_matrix = glm::rotate(transform_matrix, 
-                                       glm::radians(pitch), 
-                                       glm::vec3(0.f, 1.f, 0.f));
-        
-        transform_matrix = glm::rotate(transform_matrix, 
-                                       glm::radians(roll), 
-                                       glm::vec3(1.f, 0.f, 0.f));
-        
-        //prepare vertices
-        glm::vec3 v[8];
-        for(int i = 0; i < (int)(sizeof(v) / sizeof(v[0])); ++i)
-        {
-            glm::vec4 ext_vertice(cube_vertices[i], 1.f);
-            ext_vertice = ext_vertice * transform_matrix;
-
-            v[i] = glm::vec3(ext_vertice);
-
-            c.highlight_point(v[i]);
-        }
-
-        c.draw_rectangle(v[0], v[1], v[2], v[3], depth_f);
-        c.draw_rectangle(v[0], v[1], v[5], v[4], depth_f);
-        c.draw_rectangle(v[4], v[5], v[6], v[7], depth_f);
-        c.draw_rectangle(v[3], v[2], v[6], v[7], depth_f);
-        c.draw_rectangle(v[0], v[3], v[7], v[4], depth_f);
-        c.draw_rectangle(v[1], v[2], v[6], v[5], depth_f);
-
-        char line_char = '*';
-        c.draw_line(v[0], v[1], line_char);
-        c.draw_line(v[1], v[2], line_char);
-        c.draw_line(v[2], v[3], line_char);
-        c.draw_line(v[3], v[0], line_char);
-
-        c.draw_line(v[0 + 4], v[1 + 4], line_char);
-        c.draw_line(v[1 + 4], v[2 + 4], line_char);
-        c.draw_line(v[2 + 4], v[3 + 4], line_char);
-        c.draw_line(v[3 + 4], v[0 + 4], line_char);
-
-        c.draw_line(v[0], v[4], line_char);
-        c.draw_line(v[1], v[5], line_char);
-        c.draw_line(v[2], v[6], line_char);
-        c.draw_line(v[3], v[7], line_char);
-
-
+        cube.rotate(glm::vec3(1.f, 0.f, 0.f), pitch);
+        cube.rotate(glm::vec3(0.f, 1.f, 0.f), yaw);
+        cube.rotate(glm::vec3(0.f, 0.f, 1.f), roll);
+        cube.render(depth_f);
         
         c.render();
 
         
         //process movements for next frame
-        yaw += yaw_speed * (long int)prev_frame_time / 1000;
-        pitch += pitch_speed * (long int)prev_frame_time / 1000;
-        roll += roll_speed * (long int)prev_frame_time / 1000;
+        yaw = yaw_speed * (long int)prev_frame_time / 1000;
+        pitch = pitch_speed * (long int)prev_frame_time / 1000;
+        roll = roll_speed * (long int)prev_frame_time / 1000;
+
+        uint64_t code_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+                         std::chrono::high_resolution_clock::now() - time).count();
+
+        uint64_t sleep_time = ( code_time < frame_time_ms ) ? 
+                                frame_time_ms - code_time : 0;
+
+        std::this_thread::sleep_for(std::chrono::duration<uint64_t, std::milli>(sleep_time));
     }
 
 
